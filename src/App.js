f1c3116330dd304fdd7f7b5aab2f7c28e16250f6
Tarcísio Mesquita <tarcisiomesquitafilho@gmail.com>
2022-04-04 02:37:24 -0300

    revisao

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Rating from "./components/Rating";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Switch } from "react-router";
 import React, { useState } from "react";
-import Acesso from "./components/Acesso";
 
 function App() {
   const [user, setUser] = useState({
@@ -20,7 +19,6 @@ function App() {
     <Router>
       <Switch>
         <Route path="/" exact render={() => <Login />} />
-        <Route path="/acesso" exact render={() => <Acesso />} />
         <Route path="/turistando" render={() => <Map />} />
         <Route path="/rating" render={() => <Rating />} />
       </Switch>
